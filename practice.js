d14367b62cac63ef92af9bf91f41f292c60c78b2
Nayan <dreamerboynayan@gmail.com>
2025-07-27 22:45:38 +0600

    Practice Variable Boolean Data Type Math

---
+++ practice.js
@@ -0,0 +1,65 @@
+// Variable Practice 
+
+var name = "Nayan Mia";
+console.log(name); // Nayan Mia
+
+let address = "Dhaka, Bangladesh";
+console.log(address); // Dhaka, Bangladesh
+
+const MitusVersity = "Chattogram University";
+console.log(MitusVersity); // Chattogram University
+
+let fan = "Hania Amir";
+console.log(fan); // Hania Amir
+
+const movieOfTheYear = "Tandob";
+console.log(movieOfTheYear); // Tandob 
+
+const hobby = "Reading Books";
+console.log(hobby); // Reading Books
+
+const favColor = "Black";
+console.log(favColor); // Black
+
+// Boolean Data Type 
+
+const crushMessage = true;
+console.log(crushMessage); //true
+
+const isPhoneCharged = false;
+console.log(isPhoneCharged); // false
+
+const isDinnerDone = true;
+console.log(isDinnerDone); // true
+
+ 
+
+// Math 
+
+const earnMoney = 100;
+const costMoney = 40;
+
+const hasMoney = earnMoney - costMoney;
+console.log(hasMoney); // 60 
+
+ 
+const str1 = "Hello";
+const str2 = "World"; 
+
+const strAdd = str1 + str2;
+console.log(strAdd); // HelloWorld
+
+
+
+// Data Type Check 
+
+const price = 555;
+console.log(typeof price); // number
+
+const learning = true;
+console.log(typeof learning); // boolean 
+
+const megaCity = "Dhaka";
+console.log(typeof megaCity); // String
+
+
